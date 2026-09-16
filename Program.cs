49eb9518ff3699ce365640e0c2fcfab310782909
Vladislav44-test <elertvelert@gmail.com>
2026-09-16 18:16:40 +0300

    Додав код методу Main, який демонструє використання ключового слова is: воно дозволяє безпечно перетворювати силки на Animal силками на Hippo та Wolf, та викликає методи конкретно цих класів

--- Program.cs
+++ Program.cs
@@ -10,6 +10,21 @@ namespace MiniZooClasses
     {
         static void Main(string[] args)
         {
+            Animal[] animals =
+            {
+                new Wolf(false),
+                new Hippo(),
+                new Wolf(true),
+                new Wolf(false),
+                new Hippo()
+            };
+            foreach (Animal animal in animals)
+            {
+                animal.MakeNoise();
+                if (animal is Hippo hippo) hippo.Swim();
+                if (animal is Wolf wolf) wolf.HuntInPack();
+                Console.WriteLine();
+            }
         }
     }
 }
